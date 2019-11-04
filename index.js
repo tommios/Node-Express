6e96ce42d4dac091ce884e8ca5e72bbb419e8f9a
[tommios] <[tommios.net@gmail.com]>
2019-11-04 21:11:50 +0200

    Add Card

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-const homeRoutes = require('./routes/home')
-const coursesRoutes = require('./routes/courses')
-const addRoutes = require('./routes/add')
+const express = require('express');
+const exphbs = require('express-handlebars');
+const homeRoutes = require('./routes/home');
+const coursesRoutes = require('./routes/courses');
+const addRoutes = require('./routes/add');
+const cardRoutes = require('./routes/card');
 
-const app = express()
+const app = express();
 
 //#region HandleBars Settings Конфигурация объекта HandleBars
 
@@ -12,31 +13,32 @@ const app = express()
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
-})
+});
 
 // Регистрируем модуль hbs как движок для рендеринга html-страниц
-app.engine('hbs', hbs.engine)
+app.engine('hbs', hbs.engine);
 
 // Указываем дополнительные параметры view engine и views
-app.set('view engine', 'hbs')
-app.set('views', 'views')
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
 // Регистрируем папку public как статическую
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 // Указываем роуты на страницы
-app.use('/', homeRoutes)
-app.use('/courses', coursesRoutes)
-app.use('/add', addRoutes)
+app.use('/', homeRoutes);
+app.use('/courses', coursesRoutes);
+app.use('/add', addRoutes);
+app.use('/card', cardRoutes);
 
 
 //#endregion
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
-})
+});
---
+++ models/card.js
@@ -0,0 +1,6 @@
+class Card {
+
+}
+
+
+module.exports = Card;
