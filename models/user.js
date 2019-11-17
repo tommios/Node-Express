d98d46fe412cdd1e2aee8289b971525f78906179
tommios <tommios.net@gmail.com>
2019-11-17 20:14:31 +0200

    Refactoring code_03

--- models/user.js
+++ models/user.js
@@ -31,7 +31,25 @@ const userSchema = new Schema({
 // Определяем метод добавления в корзину, 
 // который выносится прямо в объект пользователя
 userSchema.methods.addToCart = function (course) {
+    //Клонируем массив items
+    // const items = this.cart.items.concat();
+    const clonedItems = [...this.cart.items];         // То же самое, синтаксис ES6
+    const index = clonedItems.findIndex(c => {
+        return c.courseId.toString() === course._id.toString();
+    })
 
+    if (index >= 0) {
+        clonedItems[index].count = clonedItems[index].count + 1; // Если такой курс уже есть в корзине
+    }
+    else {
+        clonedItems.push({
+            count: 1,
+            courseId: course._id
+        });
+    }
+
+    const newCart = { items: clonedItems };
+    this.cart = newCart;
 };
 
 module.exports = model('User', userSchema);
