ee2cd3941dace64960c3db45933c5fffb852836f
tommios <tommios.net@gmail.com>
2019-11-21 10:31:34 +0200

    Edit routes and views Order

--- routes/orders.js
+++ routes/orders.js
@@ -3,11 +3,27 @@ const Order = require('../models/order');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.render('orders', {
-        title: 'Заказы',
-        isOrders: true
-    })
+router.get('/', async (req, res) => {
+    const orders = await Order.find({ 'user.userId': req.user._id })
+        .populate('userId');
+
+    try {
+        res.render('orders', {
+            title: 'Заказы',
+            isOrders: true,
+            orders: orders.map(ord => {
+                return {
+                    ...ord._doc,
+                    price: ord.courses.reduce((total, c) => {
+                        return total += c.count * c.course.price
+                    }, 0)
+                }
+            })
+        });
+    }
+    catch (err) {
+        console.log(err);
+    }
 });
 
 router.post('/', async (req, res) => {
