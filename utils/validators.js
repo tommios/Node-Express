94aa6f8418f57a83bd2ce86e09ac823b97fe8e75
tommios <tommios.net@gmail.com>
2019-12-22 12:44:16 +0200

    Improving Data with Sanitizers (https://github.com/validatorjs/validator.js)

--- utils/validators.js
+++ utils/validators.js
@@ -2,23 +2,33 @@ const { body } = require('express-validator');
 const User = require('../models/user');
 
 exports.registerValidators = [
-    body('email').isEmail().withMessage('Введите корректный email').custom(async (value, { req }) => {
-        try {
-            const user = await User.findOne({ email: value });
-            if (user) {
-                return Promise.reject('Такой email уже занят');
+    body('email')
+        .isEmail().withMessage('Введите корректный email')
+        .custom(async (value, { req }) => {
+            try {
+                const user = await User.findOne({ email: value });
+                if (user) {
+                    return Promise.reject('Такой email уже занят');
+                }
             }
-        }
-        catch (err) {
-            console.log(err);
-        }
-    }),
-    body('password', 'Пароль должен быть минимум 6 символов').isLength({ min: 6, max: 56 }).isAlphanumeric(),
-    body('confirm').custom((value, { req }) => {
-        if (value !== req.body.password) {
-            throw new Error('Пароли должны совпадать');
-        }
-        return true;
-    }),
-    body('name').isLength({ min: 3 }).withMessage('Имя должно быть минимум три символа')
+            catch (err) {
+                console.log(err);
+            }
+        })
+        .normalizeEmail(),
+    body('password', 'Пароль должен быть минимум 6 символов')
+        .isLength({ min: 6, max: 56 })
+        .isAlphanumeric()
+        .trim(),
+    body('confirm')
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Пароли должны совпадать');
+            }
+            return true;
+        })
+        .trim(),
+    body('name')
+        .isLength({ min: 3 }).withMessage('Имя должно быть минимум три символа')
+        .trim()
 ]
